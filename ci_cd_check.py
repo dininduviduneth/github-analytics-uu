57b606cba609f1ec0ab8c5e8cba749b022dc6406
Steven01310131 <stefanos.tsabanakis@gmail.com>
2023-05-11 17:03:21 +0200

    added comments

--- ci_cd_check.py
+++ ci_cd_check.py
@@ -4,16 +4,18 @@ from pprint import pprint
 
 token="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"
 token = {'Authorization': 'token ' + token}
-for i in range (40):
+for i in range (10):
     url=f"https://api.github.com/search/repositories?q=created:>2022-05-01+archived:false&per_page=100&page=1"
     repositories = requests.get(url,headers=token).json()
     data={}
     for i in range(100):
         try:
             repo_name=repositories['items'][i]['full_name']
-            spec_repo_url=requests.get(f"https://api.github.com/repos/{repo_name}/hooks",headers=token).json()
-            pprint(spec_repo_url)  
-
+            spec_repo=requests.get(f"https://api.github.com/repos/{repo_name}/actions/runs",headers=token).json()
+            if spec_repo['total_count'] > 0:
+                print(f"The repository {repo_name} uses continuous integration.")
+            else:
+                continue 
         except:
             continue
 print(data)
--- prog_lang.py
+++ prog_lang.py
@@ -1,22 +1,29 @@
 import json 
 import requests
 from pprint import pprint
-
-token="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"
+#access token from my github account 
+token="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" 
 token = {'Authorization': 'token ' + token}
+#api call to get the json which just returns the list of repositories 
 url=f"https://api.github.com/search/repositories?q=created:>2022-05-01+archived:false&per_page=100"
 repositories = requests.get(url,headers=token).json()
 data={}
+#iterate through the repos 
 for i in range(100):
     try:
         repo_name=repositories['items'][i]['full_name']
         language=repositories['items'][i]['language']
+        #if the repo has no code we dont analyze
+        if language==None:
+            continue
+        # api call to specific repo to find the number of commits
+        # in order to do that we paginate to show one commit per page 
+        # and from the metadata we can extact the number of the last page which would be the number of commits 
         spec_repo_url=requests.get(f"https://api.github.com/repos/{repo_name}/commits?per_page=1&page=1",headers=token)
         headers = spec_repo_url.headers
-        # pprint(headers['Link'])
         commits=int(headers['Link'].split('&page=')[-1].split(';')[0].split('>')[0])
         data[f'{repo_name}']=[language,commits]
     except:
         continue
 print(data)
-# print(repo_name)
+
