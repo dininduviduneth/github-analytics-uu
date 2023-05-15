40aa998e4efdbcef6526ed9068b901255ac6e320
Steven01310131 <stefanos.tsabanakis@gmail.com>
2023-05-15 18:07:38 +0200

    minor

--- prog_lang.py
+++ prog_lang.py
@@ -6,7 +6,7 @@ tokens=["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"]
 #access token personal github account 
 token = {'Authorization': 'token ' + tokens.pop()}
 #api call to get the json which just returns the list of repositories 
-url=f"https://api.github.com/search/repositories?q=created:>2022-05-01+created:>2022-05-01+archived:false&per_page=100"
+url=f"https://api.github.com/search/repositories?q=created:>2022-05-01+archived:false&per_page=100"
 repositories = requests.get(url,headers=token).json()
 data={}
 #iterate through the repos 
