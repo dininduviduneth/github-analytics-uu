d6a2dfbba36c5fa5450622bf3a855f88c7a2202e
Steven01310131 <stefanos.tsabanakis@gmail.com>
2023-05-20 13:18:58 +0200

    made the three consumers

---
+++ consumers/consumer_cicd.py
@@ -0,0 +1,32 @@
+import pulsar
+import json
+import requests
+from pprint import pprint
+token="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"
+# Create a pulsar client by supplying ip address and port
+client = pulsar.Client('pulsar://pulsar_container:6650')
+data={}
+# Subscribe to a topic and subscription
+consumer1 = client.subscribe('repositories_test1', subscription_name='question3')
+while True:
+    msg1= consumer1.receive()
+    repo_name=json.loads(msg1.data().decode('utf-8'))
+    #check if there exists runs if it return an empy json it means we dont have a github workflow which means no CICD
+    spec_repo=requests.get(f"https://api.github.com/repos/{repo_name}/actions/runs",headers=token).json()
+    print(f"The repository {repo_name}")
+    if spec_repo['total_count'] > 0:
+        print(f"uses continuous integration.")
+        ci_cd= True
+    else:
+        ci_cd= False
+            
+
+    ###############
+    ##mongo code###
+    ###############
+
+
+    ###this is for checking that the connect
+    data[f"{repo_name}"]=ci_cd
+    print(data)
+client.close()
---
+++ consumers/consumer_test.py
@@ -0,0 +1,32 @@
+import pulsar
+import json
+import requests
+from pprint import pprint
+token="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"
+# Create a pulsar client by supplying ip address and port
+client = pulsar.Client('pulsar://pulsar_container:6650')
+data={}
+# Subscribe to a topic and subscription
+consumer1 = client.subscribe('repositories_test1', subscription_name='question3')
+while True:
+    msg1= consumer1.receive()
+    repo_name=json.loads(msg1.data().decode('utf-8'))
+    spec_repo_contents=requests.get(f"https://api.github.com/repos/{repo_name}/contents",headers=token).json()
+    test_driven_development= False
+    for i in spec_repo_contents:
+        if "test" in i['name'].lower():
+            print(f"uses test based development.")
+            test_driven_development= True
+            
+
+   
+    
+    ###############
+    ##mongo code###
+    ###############
+
+
+    ###this is for checking that the connect
+    data[f"{repo_name}"]=test_driven_development
+    print(data)
+client.close()
